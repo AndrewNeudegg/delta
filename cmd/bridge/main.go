ec3183c0a2d1836c2cde4884b1f4def6851cfbe4
Andrew Neudegg <andrew.neudegg@finbourne.com>
2020-12-27 14:42:45 +0000

    feat: added a example framework for cli app.
    Signed-off-by: Andrew Neudegg <andrew.neudegg@finbourne.com>

--- cmd/bridge/main.go
+++ cmd/bridge/main.go
@@ -1,28 +1,93 @@
 package main
 
 import (
-	"sync"
+	"context"
+	"os"
 
-	"github.com/andrewneudegg/delta/cmd/common"
+	"github.com/andrewneudegg/delta/cmd/bridge/apputil"
+	"github.com/andrewneudegg/delta/cmd/bridge/subcmd/example"
+	"github.com/andrewneudegg/delta/pkg/probes"
+	"github.com/andrewneudegg/delta/pkg/telemetry"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	common.LoggingInit()
-	probes := common.ProbesInit()
-	common.TelemetryInit()
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	log.Info("starting application")
-
-	probes.ReadyNow()
 	app()
-
-	wg.Wait()
-	log.Warn("exiting application")
 }
 
-func app() {
-	return
+func configureLogger(verbose bool) *log.Logger {
+	logger := log.New()
+
+	logger.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+		FullTimestamp: true,
+	})
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logger.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	if verbose {
+		logger.SetLevel(log.DebugLevel)
+	} else {
+		logger.SetLevel(log.InfoLevel)
+	}
+
+	return logger
+}
+
+func configureProbes(probesEnabled bool) *probes.ProbeServer {
+	probes := probes.ProbeServer{
+		ListenAddr: ":8082",
+	}
+
+	if probesEnabled {
+		go probes.StartProbeServer()
+	}
+
+	probes.AliveNow()
+	return &probes
+}
+
+func configureTelemetryServer(telemetryEnabled bool) *telemetry.PrometheusServer {
+	prometheusServer := telemetry.PrometheusServer{
+		ListenAddr: ":8081",
+		Route:      "/metrics",
+	}
+
+	if telemetryEnabled {
+		go prometheusServer.Serve(context.TODO())
+	}
+
+	return &prometheusServer
+}
+
+func app() error {
+	var verboseMode bool
+	var telemetryEnabled bool
+	var probesEnabled bool
+
+	appState := apputil.AppState{
+		Probes:          nil,
+		Logger:          nil,
+		TelemetryServer: nil,
+	}
+
+	var rootCmd = &cobra.Command{
+		Use: "bridge",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			appState.Logger = configureLogger(verboseMode)
+			appState.Probes = configureProbes(probesEnabled)
+			appState.TelemetryServer = configureTelemetryServer(telemetryEnabled)
+			return nil
+		},
+	}
+
+	rootCmd.PersistentFlags().BoolVarP(&verboseMode, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().BoolVarP(&telemetryEnabled, "telemetry", "", false, "telemetry server")
+	rootCmd.PersistentFlags().BoolVarP(&probesEnabled, "probes", "", false, "liveness / readiness probes")
+
+	rootCmd.AddCommand(example.Cmd(&appState))
+	return rootCmd.Execute()
 }
