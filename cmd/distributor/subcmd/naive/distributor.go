334c4f6acce12696068d3000e34626337814614a
Andrew Neudegg <andrew.neudegg@finbourne.com>
2020-12-27 16:47:23 +0000

    feat: cmd args for naive distributor

--- cmd/distributor/subcmd/naive/distributor.go
+++ cmd/distributor/subcmd/naive/distributor.go
@@ -2,6 +2,7 @@ package naive
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/spf13/cobra"
 
@@ -10,14 +11,33 @@ import (
 
 // Cmd demonstrates how to configure a new subcommand.
 func Cmd(appState *apputil.AppState) *cobra.Command {
-	return &cobra.Command{
+	var eventSources []string
+	var targetServices []string
+
+	cmd := &cobra.Command{
 		Use:   "naive",
 		Short: "naively distribute events to a specific target/s",
 		Long:  `naively distribute events to a specific target/s without any leader election`,
-		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if len(eventSources) == 0 {
+				return fmt.Errorf("must specify at least one event source")
+			}
+
+			if len(targetServices) == 0 {
+				return fmt.Errorf("must specify at least one target service")
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
 			appState.Logger.Debug("starting example cmd")
 			appState.Block(context.TODO())
+			return nil
 		},
 	}
+
+	cmd.Flags().StringSliceVarP(&eventSources, "event-sources", "e", []string{}, "sources of events")
+	cmd.Flags().StringSliceVarP(&targetServices, "target-services", "t", []string{}, "the target services to send events to")
+
+	return cmd
 }
