65e1126b280d0cb8276e607a5437dd8a5d0a0a95
Andrew Neudegg <andrew.neudegg@finbourne.com>
2020-12-30 16:43:53 +0000

    fix: improve probes test.
    Signed-off-by: Andrew Neudegg <andrew.neudegg@finbourne.com>

--- pkg/probes/probes_test.go
+++ pkg/probes/probes_test.go
@@ -1,6 +1,7 @@
 package probes
 
 import (
+	"fmt"
 	"net/http"
 	"runtime"
 	"testing"
@@ -11,47 +12,49 @@ import (
 
 func Test_healthprobes(t *testing.T) {
 
+	addr := ":6080"
+
 	client := http.Client{
 		Timeout: 5 * time.Second,
 	}
 
 	probeServer := ProbeServer{
-		ListenAddr: ":8080",
+		ListenAddr: addr,
 	}
 
 	// server not launched yet
-	resp, err := client.Get("http://localhost:8080")
+	resp, err := client.Get(fmt.Sprintf("http://localhost%s", addr))
 	assert.NotNil(t, err, "error was nil but should have been timeout")
 
 	// assert.Equal(t, 500, resp.StatusCode)
 	go probeServer.StartProbeServer()
 	runtime.Gosched()
 
-	resp, err = client.Get("http://localhost:8080/liveness")
+	resp, err = client.Get(fmt.Sprintf("http://localhost%s/liveness", addr))
 	assert.Nil(t, err, "error was not nil, but should have been")
 	assert.Equal(t, 500, resp.StatusCode)
 
-	resp, err = client.Get("http://localhost:8080/readiness")
+	resp, err = client.Get(fmt.Sprintf("http://localhost%s/readiness", addr))
 	assert.Nil(t, err, "error was not nil, but should have been")
 	assert.Equal(t, 500, resp.StatusCode)
 
 	probeServer.AliveNow()
 
-	resp, err = client.Get("http://localhost:8080/liveness")
+	resp, err = client.Get(fmt.Sprintf("http://localhost%s/liveness", addr))
 	assert.Nil(t, err, "error was not nil, but should have been")
 	assert.Equal(t, 200, resp.StatusCode)
 
-	resp, err = client.Get("http://localhost:8080/readiness")
+	resp, err = client.Get(fmt.Sprintf("http://localhost%s/readiness", addr))
 	assert.Nil(t, err, "error was not nil, but should have been")
 	assert.Equal(t, 500, resp.StatusCode)
 
 	probeServer.ReadyNow()
 
-	resp, err = client.Get("http://localhost:8080/liveness")
+	resp, err = client.Get(fmt.Sprintf("http://localhost%s/liveness", addr))
 	assert.Nil(t, err, "error was not nil, but should have been")
 	assert.Equal(t, 200, resp.StatusCode)
 
-	resp, err = client.Get("http://localhost:8080/readiness")
+	resp, err = client.Get(fmt.Sprintf("http://localhost%s/readiness", addr))
 	assert.Nil(t, err, "error was not nil, but should have been")
 	assert.Equal(t, 200, resp.StatusCode)
 }
