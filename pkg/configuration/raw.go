ef88612fcb935a55f1527cd4b726a01845c6c0a5
Andrew Neudegg <andrew.neudegg@finbourne.com>
2020-12-30 22:08:59 +0000

    fix: linter issues RawConfig.
    Signed-off-by: Andrew Neudegg <andrew.neudegg@finbourne.com>

--- pkg/configuration/raw.go
+++ pkg/configuration/raw.go
@@ -2,10 +2,12 @@ package configuration
 
 import "gopkg.in/yaml.v2"
 
+// RawConfig is generally used in tests.
 type RawConfig struct {
 	ConfigData []byte
 }
 
+// Load RawConfig from []byte.
 func (c RawConfig) Load() (Container, error) {
 	container := Container{}
 
